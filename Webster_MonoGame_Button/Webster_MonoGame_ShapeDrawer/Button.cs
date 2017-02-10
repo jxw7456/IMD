8ee37da0edb5bf7809eda6349ea13b5333d7717a
jxw7456 <jxw7456@GDD-01>
2017-02-10 14:53:55 -0500

    PE- Button started

---
+++ Webster_MonoGame_Button/Webster_MonoGame_ShapeDrawer/Button.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+//JaJuan Webster
+//Professor Cascioli
+//MonoGame Button
+
+namespace Webster_MonoGame_ShapeDrawer
+{
+    class Button
+    {
+        //Attributes
+        SpriteBatch spriteBatch;
+        Texture2D image;
+        Rectangle rectangle;
+        Color color;
+
+        public Button(SpriteBatch sb, Texture2D img, Rectangle rect, Color col, int x, int y, int width, int height)
+        {
+            sb = spriteBatch;
+            img = image;
+            rect = new Rectangle(x, y, width, height);
+            col = color;
+        }
+    }
+}
---
+++ Webster_MonoGame_Button/Webster_MonoGame_ShapeDrawer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Webster_MonoGame_ShapeDrawer")]
+[assembly: AssemblyProduct("Webster_MonoGame_ShapeDrawer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1ad33b25-74b2-455b-9b71-b4c21f873fd2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

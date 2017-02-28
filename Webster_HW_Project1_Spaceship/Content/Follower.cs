aaf75ec5a6376668fd5e33ad14b850b1b0ddc9a0
JaJuan Webster <webbie2011@gmail.com>
2017-02-28 08:47:05 -0500

    Project 1 started

---
+++ Webster_HW_Project1_Spaceship/Content/Follower.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webster_HW_Project1_Spaceship.Content
+{
+    class Follower
+    {
+    }
+}

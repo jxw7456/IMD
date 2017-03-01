50c0ae44d7e1bd0bf9d052b317936a442312f260
jxw7456 <jxw7456@GDD-01>
2017-03-01 11:59:54 -0500

    project started

---
+++ Webster_HW_Project1_Spaceship/Background.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//JaJuan Webster
+//Professor Cascioli
+//Spaceship!
+
+namespace Webster_HW_Project1_Spaceship
+{
+    class Background
+    {
+
+    }
+}
--- Webster_HW_Project1_Spaceship/Game1.cs
+++ Webster_HW_Project1_Spaceship/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+//JaJuan Webster
+//Professor Cascioli
+//Spaceship!
 
 namespace Webster_HW_Project1_Spaceship
 {
